9607bfe1d0bf18841dbae3a32d194820807648de
botsdevelopers <87813456+botsdevelopers@users.noreply.github.com>
2024-02-01 18:58:42 +0000

    editted the script

--- views/script.js
+++ views/script.js
@@ -107,14 +107,12 @@ recognition.onerror = function(event) {
 }
 socket.on('voice msg', text => {
   isSpeeking = true;
-  const apiUrl = new URL("https://tts-ai-11.onrender.com/tts");
-  apiUrl.searchParams.append("text", text);
+var msg = new SpeechSynthesisUtterance();
+var voices = window.speechSynthesis.getVoices();
 
-  fetch(apiUrl).then(response => response.json()).then(data => {
-    const audio = new Audio(data.audioFile);
-    audio.play();
-  }).catch(error => console.error("Error fetching audio:", error));
-
+msg.voice = voices[23]; // select the first available voice
+msg.text = text;
+speechSynthesis.speak(msg);
   const chatMessages = document.querySelector('.chat-messages');
 const allLiElements = chatMessages.getElementsByTagName("li");
 
